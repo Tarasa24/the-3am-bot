d27f7c67c43e0ed15b890fb7cdc50d026f9e3d7a
Tarasa24 <petr.gajdosik2424@gmail.com>
2018-12-28 12:22:31 +0100

    version 0.2.1b
    mySQL Connection lost: Error fix

--- mysql.js
+++ mysql.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
+var pool = mysql.createPool({
     host: process.env.CLEARDB_DATABASE_HOSTNAME,
     user: process.env.CLEARDB_DATABASE_USERNAME,
     password: process.env.CLEARDB_DATABASE_PASSWORD,
@@ -8,45 +8,52 @@ var con = mysql.createConnection({
 });
 
 var serverID = process.env.serverID;
-
-con.connect(function(err) { if (err) throw err; });
-
 module.exports = {
     Add: function(link, author_id) {
-        var sql = `INSERT INTO \`${server_id}\`(\`file_url\`, \`user_id\`) VALUES ("${link}","${author_id}");`
-        con.query(sql, function(err) {
-            if (err) throw err;
-            
+        pool.getConnection(function(err, con) {
+            var sql = `INSERT INTO \`${serverID}\`(\`file_url\`, \`user_id\`) VALUES ("${link}","${author_id}");`
+            con.query(sql, function(err) {
+                if (err) throw err;
+                con.release();
+            });
         });
     },
     ClearQueue: function() {
-        var sql = `DELETE FROM \`${server_id}\`;`
-        con.query(sql, function(err) {
-            if (err) throw err;
-            
+        pool.getConnection(function(err, con) {
+            var sql = `DELETE FROM \`${serverID}\`;`
+            con.query(sql, function(err) {
+                if (err) throw err;
+                con.release();
+            });
         });
     },
     QueueLength: async function() {
         return new Promise(function(resolve, reject) {
-            var sql = `SELECT COUNT(*) FROM \`${server_id}\`;`
-            con.query(sql, function(err, result) {
-                if (err) throw err;
-                resolve(result[0]["COUNT(*)"]);
+            pool.getConnection(function(err, con) {
+                var sql = `SELECT COUNT(*) FROM \`${serverID}\`;`
+                con.query(sql, function(err, result) {
+                    if (err) throw err;
+                    resolve(result[0]["COUNT(*)"]);
+                    con.release();
+                });
             });
         });
     },
     GetQueue: async function() {
         return new Promise(function(resolve, reject) {
-            var queue = { "link": [], "user_id": [] };
-            var sql = `SELECT * FROM \`${server_id}\`;`
-            con.query(sql, function(err, result) {
-                if (err) throw err;
-                for (e in result) {
-                    queue.link.push(result[e].file_url)
-                    queue.user_id.push(result[e].user_id)
-                }
-                resolve(queue);
+            pool.getConnection(function(err, con) {
+                var queue = { "link": [], "user_id": [] };
+                var sql = `SELECT * FROM \`${serverID}\`;`
+                con.query(sql, function(err, result) {
+                    if (err) throw err;
+                    for (e in result) {
+                        queue.link.push(result[e].file_url)
+                        queue.user_id.push(result[e].user_id)
+                    }
+                    resolve(queue);
+                    con.release();
+                });
             });
-        })
+        });
     }
 }
